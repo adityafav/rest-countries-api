a27a4d8d4e6af782de94a442360e2c6820604d2d
Aditya Singh <adityafav@gmail.com>
2021-01-18 23:52:05 +0530

    18-01-2021

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,31 @@
 const countries = document.querySelector('#countries');
 const toggle_icon = document.querySelector('#toggle_mode i');
 const toggle_buttons = document.querySelectorAll('#toggle_mode button');
+const body = document.querySelector('body');
+
+
+var country_card = (flag,name,population,region,capital) => {
+    return '<div class="country-card col-12 col-sm-6 col-md-4 col-lg-3 mb-5 rounded">' +
+                '<div class="country-card-content pb-5 rounded">' +
+                    '<div class="country-image">' +
+                        '<img src=' + flag + ' class="rounded-top w-100 h-100">' +
+                    '</div>'+
+                    '<div class="country-info ml-4">' +
+                        '<p class="my-4"><strong>' + name +'</strong></p>'+
+                        '<p class="mb-1"><strong>Population</strong>: ' + population +'</p>'+
+                        '<p class="mb-1"><strong>Region</strong>: ' + region +'</p>'+
+                        '<p class=""><strong>Capital</strong>: ' + capital +'</p>'+
+                    '</div>'+
+                '</div>' +
+            '</div>' ;
+}
 
 const home = () => {
     axios.get('https://restcountries.eu/rest/v2/all')
     .then(countries.innerHTML = "")
     .then(res => {
-        res.data.forEach(element => {
-            countries.innerHTML += 
-            '<div class="country-card col-12 col-sm-6 col-md-4 col-lg-3 mb-5 rounded">' +
-                '<div class="country-card-content pb-5 rounded">' +
-                    '<img src=' + element.flag + ' class="rounded-top w-100">' +
-                    '<div class="country-info ml-4">' +
-                        '<h5 class="my-4">' + element.name +'</h5>'+
-                        '<p class="mb-1"><strong>Population</strong>: ' + element.population +'</p>'+
-                        '<p class="mb-1"><strong>Region</strong>: ' + element.region +'</p>'+
-                        '<p class=""><strong>Capital</strong>: ' + element.capital +'</p>'+
-                    '</div>'+
-                '</div>' +
-            '</div>' ;
+        res.data.forEach(e => {
+            countries.innerHTML += country_card(e.flag, e.name, e.population, e.region, e.capital);
         });
     })
 }
@@ -28,18 +35,7 @@ const region_select = (region) => {
     .then(countries.innerHTML = "")
     .then(res => {
         res.data.forEach(element => {
-            countries.innerHTML += 
-            '<div class="country-card col-12 col-sm-6 col-md-4 col-lg-3 mb-5 rounded">' +
-                '<div class="country-card-content pb-5 rounded">' +
-                    '<img src=' + element.flag + ' class="rounded-top w-100">' +
-                    '<div class="country-info ml-4">' +
-                        '<h5 class="my-4">' + element.name +'</h5>'+
-                        '<p class="mb-1"><strong>Population</strong>: ' + element.population +'</p>'+
-                        '<p class="mb-1"><strong>Region</strong>: ' + element.region +'</p>'+
-                        '<p class=""><strong>Capital</strong>: ' + element.capital +'</p>'+
-                    '</div>'+
-                '</div>' +
-            '</div>' ;
+            countries.innerHTML += country_card(e.flag, e.name, e.population, e.region, e.capital);
         });
     })
 }
@@ -52,18 +48,7 @@ const search = (name) => {
     .then(countries.innerHTML = "")
     .then(res => {
         res.data.forEach(element => {
-            countries.innerHTML += 
-            '<div class="country-card col-12 col-sm-6 col-md-4 col-lg-3 mb-5 rounded">' +
-                '<div class="country-card-content pb-5 rounded">' +
-                    '<img src=' + element.flag + ' class="rounded-top w-100">' +
-                    '<div class="country-info ml-4">' +
-                        '<h5 class="my-4">' + element.name +'</h5>'+
-                        '<p class="mb-1"><strong>Population</strong>: ' + element.population +'</p>'+
-                        '<p class="mb-1"><strong>Region</strong>: ' + element.region +'</p>'+
-                        '<p class=""><strong>Capital</strong>: ' + element.capital +'</p>'+
-                    '</div>'+
-                '</div>' +
-            '</div>' ;
+            countries.innerHTML += country_card(e.flag, e.name, e.population, e.region, e.capital);
         });
     })
 }
@@ -74,4 +59,5 @@ const toggle_mode = () => {
     toggle_buttons.forEach(e => {
         e.classList.toggle('hidden');
     })
+    body.classList.toggle('darkmode');
 }
