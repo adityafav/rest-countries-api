b8db709915cdb6bbe8e6cf141500f4ea5392d92b
Aditya Singh <adityafav@gmail.com>
2021-01-18 17:37:44 +0530

    18-01-2021

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,11 @@
 const countries = document.querySelector('#countries');
 
-axios.get('https://restcountries.eu/rest/v2/all')
+
+const home = () => {
+    axios.get('https://restcountries.eu/rest/v2/all')
+    .then(countries.innerHTML = "")
     .then(res => {
         res.data.forEach(element => {
-            console.log(element.name)
             countries.innerHTML += '<div class="country-card col-12 col-sm-6 col-md-4 col-lg-3 mb-5 rounded">' +
             '<img src='+ element.flag + ' class="rounded-top w-100">' +
             '<div class="country-info ml-4">' +
@@ -15,3 +17,43 @@ axios.get('https://restcountries.eu/rest/v2/all')
         '</div>' ; 
         });
     })
+}
+
+const region_select = (region) => {
+    axios.get('https://restcountries.eu/rest/v2/region/'+ region)
+    .then(countries.innerHTML = "")
+    .then(res => {
+        res.data.forEach(element => {
+            countries.innerHTML += '<div class="country-card col-12 col-sm-6 col-md-4 col-lg-3 mb-5 rounded">' +
+            '<img src='+ element.flag + ' class="rounded-top w-100">' +
+            '<div class="country-info ml-4">' +
+                '<h5 class="my-4">'+ element.name +'</h5>'+
+                '<p class="mb-1"><strong>Population</strong>:'+ element.population +'</p>'+
+                '<p class="mb-1"><strong>Region</strong>:'+ element.region +'</p>'+
+                '<p class="mb-5"><strong>Capital</strong>:'+ element.capital +'</p>'+
+            '</div>'+
+        '</div>' ; 
+        });
+    })
+}
+
+const search = (name) => {
+    if(name === ""){
+        home();
+    }
+    axios.get('https://restcountries.eu/rest/v2/name/'+ name)
+    .then(countries.innerHTML = "")
+    .then(res => {
+        res.data.forEach(element => {
+            countries.innerHTML += '<div class="country-card col-12 col-sm-6 col-md-4 col-lg-3 mb-5 rounded">' +
+            '<img src='+ element.flag + ' class="rounded-top w-100">' +
+            '<div class="country-info ml-4">' +
+                '<h5 class="my-4">'+ element.name +'</h5>'+
+                '<p class="mb-1"><strong>Population</strong>:'+ element.population +'</p>'+
+                '<p class="mb-1"><strong>Region</strong>:'+ element.region +'</p>'+
+                '<p class="mb-5"><strong>Capital</strong>:'+ element.capital +'</p>'+
+            '</div>'+
+        '</div>' ; 
+        });
+    })
+}
