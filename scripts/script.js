a32edb410582bc89aa6cc3a803519ac546cbd4ad
Aditya Singh <adityafav@gmail.com>
2021-01-21 00:46:08 +0530

    21-01-2021

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ const toggle_icon = document.querySelector('#toggle_mode i');
 const toggle_buttons = document.querySelectorAll('#toggle_mode button');
 const body = document.querySelector('body');
 const api = "https://restcountries.eu/rest/v2/";
-const fields = "?fields=name;flag;population;region;capital;nativeName;subregion;topLevelDomain;currencies;languages;borders"
+const fields = "?fields=name;flag;population;region;capital;nativeName;subregion;topLevelDomain;currencies;languages;borders;alpha3Code"
 
 
 // get api data (only once)
@@ -90,25 +90,41 @@ const details = (arg) => {
 
 
 // details page
-const detailflag = document.querySelector('#flag');
-var country = sessionStorage.getItem("country")
-
-var detail_card = (flag) =>{
-    return '<img src="'+ flag + '" class="w-100"></img>'
-}
-const insert_detail = (res) =>{
-    res.data.forEach(e => {
-        detailflag.innerHTML += detail_card(e.flag);
-    });
-}
+const cflag = document.querySelector('#country_flag');
+const cname = document.querySelector('#country_name');
+const cdetails = document.querySelector('#country_details');
+const cborders = document.querySelector('#country_borders');
 
 
 const details_load = () => {
     let country = sessionStorage.getItem("country");
-    axios.get( api + 'name/' + country + '?fullText=true' + fields)
-    .then(flag.innerHTML = "")
-    .then(res => {
-        insert_detail(res);
-    })
+
+    data.forEach(e => {
+        if(e.name === country){
+            cname.innerHTML = e.name;
+            cflag.innerHTML = '<img src="'+ e.flag + '" class="w-100"></img>' ;
+            cdetails.innerHTML = '<div class="col-12 col-lg-6">' +
+                    '<p class="mb-1"><strong>Native Name</strong> : '+ e.nativeName +'</p>'+
+                    '<p class="mb-1"><strong>Population</strong> : '+ e.population +'</p>'+
+                    '<p class="mb-1"><strong>Region</strong> : '+ e.region +'</p>'+
+                    '<p class="mb-1"><strong>Sub Region</strong> : '+ e.subregion +'</p>'+
+                    '<p class="mb-1"><strong>Capital</strong> : ' + e.capital +'</p>'+
+                '</div>'+
+                '<div class="col-12 col-lg-6">'+
+                    '<p class="mb-1"><strong>Top Level Domain</strong> : '+ e.topLevelDomain[0] +'</p>'+
+                    '<p class="mb-1"><strong>Currencies</strong> : '+ e.currencies[0].name +' </p>'+
+                    '<p class="mb-1"><strong>Languages</strong> : '+ e.languages[0].name +' </p>'+
+                '</div>'
+            cborders.innerHTML = '<p><strong>Border Countries: </strong></p>';
+
+            e.borders.forEach(code =>{
+                data.forEach(ctry =>{
+                    if(code === ctry.alpha3Code){
+                        cborders.innerHTML += '<a href="detail.html"><button class="borders mb-3 mr-2" id="'+ ctry.name +'" onclick="details(id)">'+ ctry.name +'</button></a>'
+                    }
+                });
+            });
+        }
+    });
 
 }
